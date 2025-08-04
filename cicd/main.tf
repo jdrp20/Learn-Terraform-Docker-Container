1e4440df27a1d35972781100cc9a45cb2da75327
jdrp20 <jonathanreyes2050@gmail.com>
2025-08-04 12:47:02 -0500

    utilizacion del import

--- cicd/main.tf
+++ cicd/main.tf
@@ -8,17 +8,17 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus2"
 }
 
-import {
-  to = azurerm_resource_group.rg
-  id = "/subscriptions/e973f371-80ef-4813-9adc-d4c2f2b1c972/resourceGroups/cicd-group/providers/Microsoft.Storage/storageAccounts/continousdeployment"
-}
-
 resource "azurerm_storage_account" "storage" {
   name                     = "continousdeployment"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+import {
+  to = azurerm_resource_group.rg
+  id = "/subscriptions/e973f371-80ef-4813-9adc-d4c2f2b1c972/resourceGroups/cicd-group/providers/Microsoft.Storage/storageAccounts/continousdeployment"
 }
 
 resource "azurerm_storage_container" "example" {
