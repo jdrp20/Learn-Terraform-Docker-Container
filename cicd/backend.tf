521eb8fb48f0def4c9ac604cd9802655cbab0aeb
jdrp20 <jonathanreyes2050@gmail.com>
2025-08-04 12:32:38 -0500

    cambio en estadosterraformjony para que se ejecute bien

--- cicd/backend.tf
+++ cicd/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "estadosterraforjony"
+    storage_account_name = "estadosterraformjony"
     container_name       = "githubactionstate"
     key                  = "estados.tfstate"
   }
