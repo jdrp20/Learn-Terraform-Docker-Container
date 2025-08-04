dc82893cfa130a95ec9d239ee88780924941f519
jdrp20 <jonathanreyes2050@gmail.com>
2025-08-04 12:41:00 -0500

    utilizacion del import

--- cicd/main.tf
+++ cicd/main.tf
@@ -8,6 +8,11 @@ resource "azurerm_resource_group" "rg" {
   location = "eastus2"
 }
 
+import {
+  to = azurerm_resource_group.rg
+  id = "subscriptions/e973f371-80ef-4813-9adc-d4c2f2b1c972/resourceGroups/cicd-group"
+}
+
 resource "azurerm_storage_account" "storage" {
   name                     = "continousdeployment"
   resource_group_name      = azurerm_resource_group.rg.name
